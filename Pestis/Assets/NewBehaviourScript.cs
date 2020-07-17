1cff4495b7e83573a2a3d9c2691c2c2579ecd336
cso1999888 <66512887+cso1999888@users.noreply.github.com>
2020-07-17 23:29:59 +0800

    20200717_HW4

---
+++ Pestis/Assets/NewBehaviourScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    #region Data
+    [Tooltip("攻擊力")]
+    public int ATK = 10;
+    [Tooltip("防禦力")]
+    public int DEF = 10;
+    [Tooltip("血量")]
+    public int HP = 1000;
+    [Tooltip("魔力")]
+    public int MP = 300;
+    [Tooltip("技能一")]
+    public string SkillOne = "火";
+    [Tooltip("技能一消耗魔力")]
+    public int SkillOneMp = 50;
+    [Tooltip("技能二")]
+    public string SkillTwo = "水";
+    [Tooltip("技能二消耗魔力")]
+    public int SkillTwoMp = 50;
+    bool Death;
+    [Tooltip("移動速度")]
+    public int MS = 5;
+    #endregion
+    #region Methods
+    void Move()
+    {
+        print("move");
+    }
+    void CastSkillOne()
+    {
+
+    }
+    void CastSkillTwo()
+    {
+
+    }
+    void Hit()
+    {
+
+    }
+    void Die()
+    {
+
+    }
+    void Attack()
+    {
+
+    }
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Move();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
